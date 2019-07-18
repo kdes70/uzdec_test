73a0ab6b8992af57fb8263350f85ff9df61a0f0f
kdes70 <kdes70@mail.ru>
2019-07-18 18:51:02 +0700

    git init

---
+++ app/app/User.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password', 'state', 'role',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    const STATE_WAIT = 'wait'; // в ожидание
+    const STATE_ACTIVE = 'active'; // астивен
+    const STATE_BANNED = 'banned'; // забанен на время
+
+    const ROLE_USER = 'user';
+    const ROLE_ADMIN = 'admin';
+}
